858a7738718827a4bce01f2ef004f81786ef2140
Piplsingha <120404325+Piplsingha@users.noreply.github.com>
2023-10-02 22:05:28 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// join_our_team section cerasul 
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  items:2,
+  margin:10,
+  nav:true,
+  autoplay:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  autoplayHoverPause:true
+});
